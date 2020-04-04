7578c1d597306707b00e588ab5565c86c828dab8
Shane Kaestner <shane.kaestner@yahoo.com>
2020-04-03 18:54:17 -0700

    debugged the options.js file

--- options.js
+++ options.js
@@ -1,7 +1,7 @@
 let page = document.getElementById('buttonDiv');
 const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-function constructOption(kButtonColors){
-    for (let item of kButtonColor){
+function constructOptions(kButtonColors){
+    for (let item of kButtonColors){
         let button = document.createElement('button');
         button.style.backgroundColor = item;
         button.addEventListener('click', function(){
